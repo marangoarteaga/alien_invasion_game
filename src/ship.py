c30c280e7b6d0ff98a88e7a39513906a082951a0
Marangoa <mauricioarango2@gmail.com>
2026-03-06 20:39:27 -0500

    Se crea la clase Ship.

---
+++ src/ship.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Ship:
+    '''Una clase para gestionar la nave.'''
+
+    def __init__(self, ai_game):
+        '''Inicializa la nave y configura su posición inicial.'''
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        #Carga la imagen de la nave y obtiene su rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        #Coloca inicialmente cada nave nueva en el centro de la parte inferior de la pantalla.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        '''Dibuja la nave en su ubicación actual.'''
+        self.screen.blit(self.image, self.rect)
