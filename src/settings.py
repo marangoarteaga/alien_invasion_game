ce027f23567308a3184a807cab7934a1f079ee64
Marangoa <mauricioarango2@gmail.com>
2026-03-06 21:49:41 -0500

    Ajusta la configuración de la pantalla: modifica el ancho y alto a 1280x720

--- src/settings.py
+++ src/settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         '''Inicializa la configuración del juego'''
         #Configuración de la pantalla:
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1280
+        self.screen_height = 720
         self.bg_color = (230, 230, 230)
