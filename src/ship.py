5e00395475dd59c9b9aa1c964d2c7d305196101e
Marangoa <mauricioarango2@gmail.com>
2026-03-06 21:39:52 -0500

    Añadir mensaje de carga al inicializar la imagen de la nave

--- src/ship.py
+++ src/ship.py
@@ -9,6 +9,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         #Carga la imagen de la nave y obtiene su rect.
+        print("Cargando imagen...")
         self.image = pygame.image.load('assets/images/ship.bmp')
         self.rect = self.image.get_rect()
 
