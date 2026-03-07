e7f7dfc45ee251cb2776e6e8bef7ce456387a9af
Marangoa <mauricioarango2@gmail.com>
2026-03-06 19:23:39 -0500

    Importar Settings: línea 4 Crear instancia Settings y asignarla a self.settings: linea 13. Usar los atributos screen width y screen height de self.settings: línea 15. Usar self.settings para acceder al color de fondo: linea 27

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 
+from settings import Settings
+
 class AlienInvasion:
     '''Clase general para gestionar los recursos y el comportamiento del juego'''
 
@@ -8,12 +10,11 @@ class AlienInvasion:
         '''Inicializa el juego y crea recursos del juego'''
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
         pygame.display.set_caption("Alien Invasion")
-
-        self.bg_color = (230, 230, 230)
-        '''Configura el color de fondo.'''
 
     def run_game(self):
         '''Inicia el bucle principal para el juego'''
@@ -23,7 +24,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             '''Redibuja la patalla en cada paso por el bucle'''
             pygame.display.flip()
             '''Hace visible la última pantalla dibujada.'''
