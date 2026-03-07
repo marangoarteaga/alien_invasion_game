13461f3d7b34038c045aa1e5ba3f831be7d4ecd2
Marangoa <mauricioarango2@gmail.com>
2026-03-06 19:03:36 -0500

    Se crea el directorio settings.py para añadir la clase Settings.

---
+++ src/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    '''Una clase para guardar toda la configuración de Alien Invasion'''
+
+    def __init__(self):
+        '''Inicializa la configuración del juego'''
+        #Configuración de la pantalla:
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
